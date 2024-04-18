d65745c4112e4c3855b24872c9dec42c103e0a99
FMI <fmi@orgadata.com>
2024-04-18 11:22:19 +0200

    add nvim config
    add nvim config

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "skywind3000/asyncrun.vim",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "Shougo/neosnippet",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "godlygeek/tabular",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "mattia72/vim-delphi",
+    config = function()
+      require "configs.conform"
+    end,
+  }
+
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+  --
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
